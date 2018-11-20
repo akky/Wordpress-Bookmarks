2d8d94e0aa031cac5f1dca9c295f956280e2634f
akky <akimoto+github@gmail.com>
2018-11-21 08:43:52 +0900

    removed 'require' included wp-config.php again. checked array access with non-existing key.

--- bookmarks.php
+++ bookmarks.php
@@ -9,7 +9,7 @@
  */
 
 // important that this plugin is in standard install directory, i.e. wp-content/plugins/pluginname
-require (dirname(__FILE__).'/../../../wp-config.php');
+//require (dirname(__FILE__).'/../../../wp-config.php');
 // add the admin options page stuff
 // require_once ('pm_admin_menu.php');
 
@@ -18,38 +18,39 @@ add_action('save_post', 'pk_save_bookmark');
 
 function pk_create_bookmark_post_type() {
     register_post_type('bookmark', array(
-		'label'=>'Bookmark', 
-		'labels'=>array(
-			'name'=>_x('Bookmarks', 'post type general name'), 
-			'singular_name'=>_x('Bookmark', 'post type singular name'), 
-			'add_new'=>_x('Add New', 'pk_bookmark'), 
-			'add_new_item'=>__('Add New Bookmark'),
-			'edit_item'=>__('Edit Bookmark'), 
-			'new_item'=>__('New Bookmark'), 
-			'view_item'=>__('View Bookmark'), 
-			'search_items'=>__('Search Bookmarks'), 
-			'not_found'=>__('No bookmarks found'), 
-			'not_found_in_trash'=>__('No bookmarks found in Trash'), 
-			'parent_item_colon'=>''), 
-		'public'=>true, 
-		'publicly_queryable'=>true, 
-		'show_ui'=>true, 
-		'query_var'=>true, 
-		'rewrite'=>false, 
-		'capability_type'=>'post',
-    	'supports'=>array('title','thumbnail'),
-    	'taxonomies'=>array('category', 'post_tag'),
-		'slug'=>'bookmark',
-		'hierarchical'=>false, 
-		'menu_position'=>4
-	));
+        'label'=>'Bookmark', 
+        'labels'=>array(
+            'name'=>_x('Bookmarks', 'post type general name'), 
+            'singular_name'=>_x('Bookmark', 'post type singular name'), 
+            'add_new'=>_x('Add New', 'pk_bookmark'), 
+            'add_new_item'=>__('Add New Bookmark'),
+            'edit_item'=>__('Edit Bookmark'), 
+            'new_item'=>__('New Bookmark'), 
+            'view_item'=>__('View Bookmark'), 
+            'search_items'=>__('Search Bookmarks'), 
+            'not_found'=>__('No bookmarks found'), 
+            'not_found_in_trash'=>__('No bookmarks found in Trash'), 
+            'parent_item_colon'=>''), 
+        'public'=>true, 
+        'publicly_queryable'=>true, 
+        'show_ui'=>true, 
+        'query_var'=>true, 
+        'rewrite'=>false, 
+        'capability_type'=>'post',
+        'supports'=>array('title','thumbnail'),
+        'taxonomies'=>array('category', 'post_tag'),
+        'slug'=>'bookmark',
+        'hierarchical'=>false, 
+        'menu_position'=>4
+    ));
 }
 
 function pk_save_bookmark() {
     global $post;
-    if ($_POST['pk_bookmark_link_url'])
+    if (array_key_exists('pk_bookmark_link_url', $_POST) && $_POST['pk_bookmark_link_url'])
         update_post_meta($post->ID, 'link_url', $_POST['pk_bookmark_link_url'], false);
-    if ($_POST['pk_bookmark_link_desc'])
+//    if ($_POST['pk_bookmark_link_desc'])
+    if (array_key_exists('pk_bookmark_link_desc', $_POST) && $_POST['pk_bookmark_link_desc'])
         update_post_meta($post->ID, 'link_desc', $_POST['pk_bookmark_link_desc'], false);
 }
 
@@ -61,9 +62,9 @@ function pk_bookmark_metabox($post) {
     $custom = get_post_custom($post->ID);
     
     echo '<table width="100%">';
-	    echo '<tr><td width="110"><b>URL:</b></td><td><input type="text" name="pk_bookmark_link_url" value="'.($custom && $custom['link_url'] ? htmlspecialchars(array_pop($custom['link_url'])) : '').'" style="width: 100%"/></td></tr>';
-	    echo '<tr><td><b>Description:</b></td><td><input type="text" name="pk_bookmark_link_desc" value="'.($custom && $custom['link_desc'] ? htmlspecialchars(array_pop($custom['link_desc'])) : '').'" style="width: 100%"/></td></tr>';
-	echo '</table>';
+        echo '<tr><td width="110"><b>URL:</b></td><td><input type="text" name="pk_bookmark_link_url" value="'.($custom && $custom['link_url'] ? htmlspecialchars(array_pop($custom['link_url'])) : '').'" style="width: 100%"/></td></tr>';
+        echo '<tr><td><b>Description:</b></td><td><input type="text" name="pk_bookmark_link_desc" value="'.($custom && $custom['link_desc'] ? htmlspecialchars(array_pop($custom['link_desc'])) : '').'" style="width: 100%"/></td></tr>';
+    echo '</table>';
     
 ?>
 <script type="text/javascript">
